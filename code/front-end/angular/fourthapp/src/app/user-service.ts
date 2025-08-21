d962f41dcab509b0e294367e35b3e9ebdf1678bf
jayanta1971 <jk_tin@yahoo.com>
2025-08-21 17:07:11 +0530

    added

--- code/front-end/angular/fourthapp/src/app/user-service.ts
+++ code/front-end/angular/fourthapp/src/app/user-service.ts
@@ -15,5 +15,20 @@ export class UserService {
     return this.http.get<IUser[]>('http://localhost:3000/user');
   }
 
+    getUsersById(userId:string):Observable<IUser> {
+    return this.http.get<IUser>('http://localhost:3000/user/' + userId);
+  }
+
+  updateUser(user: IUser): Observable<IUser> {
+    return this.http.put<IUser>(`http://localhost:3000/user/${user.id}`, user);
+  }
+
+  deleteUser(userId: string): Observable<void> {
+    return this.http.delete<void>(`http://localhost:3000/user/${userId}`);
+  }
+
+  saveUser(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>('http://localhost:3000/user', user);
+  }
 
 }
--- code/front-end/angular/fourthapp/src/app/usercomp/usercomp.ts
+++ code/front-end/angular/fourthapp/src/app/usercomp/usercomp.ts
@@ -1,27 +1,106 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user-service';
 import { IUser } from '../iuser';
+import { FormsModule } from '@angular/forms';
 
+
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-usercomp',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './usercomp.html',
   styleUrl: './usercomp.css'
 })
 export class Usercomp {
 
-  constructor(private userService:UserService) { }
+  constructor(private userService:UserService, private cdr:ChangeDetectorRef) { }
 
   users: IUser[] = [];
+  userEdit: IUser = {
+    id: '',
+    userName: '',
+    email: '',
+    password: ''
+  };
+  userAdd: IUser = {
+    id: '',
+    userName: '',
+    email: '',
+    password: ''
+  };
 
   ngOnInit() {
-    this.userService.getUsers().subscribe((data: IUser[]) => {
+  this.userService.getUsers().subscribe((data: IUser[]) => {
+    console.log('Data received:', data); // Check if data is received on refresh
+    this.users = data;
+    this.cdr.detectChanges();
+    console.log('Users fetched successfully:', this.users);
+  }, (error) => {
+    console.error('Error fetching users:', error);
+  });
+}
 
-      this.users = data;
-      console.log('Users fetched successfully:',  this.users);
+  edit(userId: string) {
+    console.log('Edit user with ID:', userId);
+    this.userService.getUsersById(userId).subscribe((user: IUser) => {
+      console.log('User details:', user);
+      this.userEdit = user;
+      console.log('User for editing:', this.userEdit);
+       this.cdr.detectChanges();
     }, (error) => {
-      console.error('Error fetching users:', error);
+      console.error('Error fetching user by ID:', error);
     });
   }
+update() {
+  console.log('Updating user:', this.userEdit);
+    this.userService.saveUser(this.userEdit).subscribe((updatedUser: IUser) => {
+      console.log('User updated successfully:', updatedUser);
+      const index = this.users.findIndex(user => user.id === updatedUser.id);
+      if (index !== -1) {
+        this.users[index] = updatedUser;
+      }
+      this.userEdit = {
+        id: '',
+        userName: '',
+        email: '',
+        password: ''
+      };
+      this.cdr.detectChanges();
+      this.ngOnInit();
+    }, (error) => {
+      console.error('Error updating user:', error);
+    });
+}
 
+delete(userId: string) {
+  this.userService.deleteUser(userId).subscribe(() => {
+    console.log('User deleted successfully:', userId);
+    this.users = this.users.filter(user => user.id !== userId);
+    this.cdr.detectChanges();
+    this.ngOnInit();
+  }, (error) => {
+    console.error('Error deleting user:', error);
+  });
+
+
+}
+
+save(){
+  console.log("saving data-"+this.userAdd)
+  this.userService.saveUser(this.userAdd).subscribe((newUser: IUser) => {
+    console.log('User saved successfully:', newUser);
+    this.users.push(newUser);
+    this.userAdd = {
+      id: '',
+      userName: '',
+      email: '',
+      password: ''
+    };
+    this.cdr.detectChanges();
+  }, (error) => {
+    console.error('Error saving user:', error);
+  });
+
+}
 }
